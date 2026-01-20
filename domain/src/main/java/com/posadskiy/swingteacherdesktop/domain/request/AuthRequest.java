bdcaa2553bb7646a06167f830c62766147cb1047
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2026-01-20 21:11:52 +0100

    Migration to using auth-service and user-service

--- domain/src/main/java/com/posadskiy/swingteacherdesktop/domain/request/AuthRequest.java
+++ domain/src/main/java/com/posadskiy/swingteacherdesktop/domain/request/AuthRequest.java
@@ -3,7 +3,7 @@ package com.posadskiy.swingteacherdesktop.domain.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record AuthRequest(
-    @NotBlank String login,
+    @NotBlank String username,
     @NotBlank String password
 ) {
 }
